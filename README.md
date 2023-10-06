2a5d21b8c88d6378bedad78057705f65a22e3d87
Steinar Cook <steinar.cook@autostoresystem.com>
2023-10-06 02:58:06 +0200

    Cleaned up the build.sh script

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 
+## How to build on MacOS
 
+The script `build.sh` will compile all the binaries and upload them
+to the OneDrive directory.
+
+## Cross compiling to Windows on MacOS
 How to cross compile from Mac to Windows:
 ```shell
 brew install mingw-w64
--- build.sh
+++ build.sh
@@ -6,8 +6,19 @@ cargo build --release
 cargo install --path jira_worklog
 cargo build --target x86_64-pc-windows-gnu --release
 
-echo "Deploying to shared areas..."
-cp target/release/jira_worklog /Users/steinar/Library/CloudStorage/OneDrive-SharedLibraries-AUTOSTOREAS/QubIt\ -\ Documents/042-worklog/MacOs
-cp target/x86_64-pc-windows-gnu/release/jira_worklog.exe /Users/steinar/Library/CloudStorage/OneDrive-SharedLibraries-AUTOSTOREAS/QubIt\ -\ Documents/042-worklog/Windows/jira_worklog.exe
-cp jira_worklog/README.pdf /Users/steinar/Library/CloudStorage/OneDrive-SharedLibraries-AUTOSTOREAS/QubIt\ -\ Documents/042-worklog
+# Ugly path to OneDrive on MacOS
+DEPLOY_DIR="$HOME/Library/CloudStorage/OneDrive-SharedLibraries-AUTOSTOREAS/QubIt\ -\ Documents/042-worklog/"
+
+echo "Deploying to shared areas in OneDrive ..."
+echo "$DEPLOY_DIR"
+
+if [ ! -d "$DEPLOY_DIR" ]; then
+  echo "Seems the OneDrive Directory is not available:"
+  echo "$DEPLOY_DIR"
+  exit 4
+fi
+
+cp target/release/jira_worklog $DEPLOY_DIR/MacOs
+cp target/x86_64-pc-windows-gnu/release/jira_worklog.exe $DEPLOY_DIR/Windows/jira_worklog.exe
+cp jira_worklog/README.pdf $DEPLOY_DIR
 echo "Done"
