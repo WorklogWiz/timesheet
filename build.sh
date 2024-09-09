7201294e4ef9e9e32ee68c6de1e63b369fb2cc30
Steinar Cook <steinar.cook@autostoresystem.com>
2024-09-09 16:32:06 +0200

    Fixed a bug in build.sh to handle OneDrive directories with embedded blanks.

--- build.sh
+++ build.sh
@@ -7,12 +7,12 @@ cargo install --path jira_worklog
 cargo build --target x86_64-pc-windows-gnu --release
 
 # Ugly path to OneDrive on MacOS
-DEPLOY_DIR="$HOME/Library/CloudStorage/OneDrive-SharedLibraries-AUTOSTOREAS/QubIt\ -\ Documents/042-worklog/"
+DEPLOY_DIR="$HOME/Library/CloudStorage/OneDrive-SharedLibraries-AUTOSTOREAS/QubIt - Documents/042-worklog/"
 
 echo "Deploying to shared areas in OneDrive ..."
 echo "$DEPLOY_DIR"
 
-if [ ! -d "$DEPLOY_DIR" ]; then
+if [[ ! -d "$DEPLOY_DIR" ]]; then
   echo "Seems the OneDrive Directory is not available:"
   echo "$DEPLOY_DIR"
   exit 4
