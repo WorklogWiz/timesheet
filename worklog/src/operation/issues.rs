900e9376fa1c0a7cdd214e73816c3000401205eb
Steinar Overbeck Cook <steinar.cook@gmail.com>
2024-12-25 21:09:39 +0100

    Refactor issue retrieval logic in `execute` function.
    Replaced `get_issues_for_project` with `get_issue_summaries` for improved flexibility and functionality. This change allows querying with additional parameters and enhances code clarity.

--- worklog/src/operation/issues.rs
+++ worklog/src/operation/issues.rs
@@ -4,8 +4,7 @@ use crate::{error::WorklogError, ApplicationRuntime};
 
 pub(crate) async fn execute(runtime: &ApplicationRuntime) -> Result<Vec<IssueSummary>, WorklogError> {
     let jira_client = runtime.jira_client();
-    let issues = jira_client
-        .get_issues_for_project("TIME".to_string())
+    let issues = jira_client.get_issue_summaries(&vec!["TIME"], &vec![],false)
         .await?;
 
     Ok(issues)
